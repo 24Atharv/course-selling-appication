657d57a5b993d02159bebe656540561a52b52575
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-13 06:22:31 +0000

    start ti build

--- Bookmark Manager/index.js
+++ Bookmark Manager/index.js
@@ -1,2 +1,27 @@
-console.log("hii there");
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const bookmarks = [];
+
+app.post('/', (req, res) => {
+    const url = req.body.url
+    const category = req.body.category
+
+    bookmarks.push({
+        url: url,
+        category: category
+    });
+
+    console.log(bookmarks);
+
+    res.json({
+        message: "Bookmark added"
+    })
+});
+
+
+
+app.listen(3000, () => console.log("Server Started"));
 
