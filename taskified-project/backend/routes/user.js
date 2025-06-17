192758f5d402af0db4074b9e4291dfd782517441
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-17 16:15:30 +0000

    userRouter added

--- taskified-project/backend/routes/user.js
+++ taskified-project/backend/routes/user.js
@@ -1,12 +1,51 @@
+require('dotenv').config();
 const { Router } = require('express');
-const userRouter = Router()
+const { userModel } = require('../../db');
+const { jwt } = require('jsonwebtoken');
+const userRouter = Router();
 
-userRouter.post('/signup', (req, res) => {
+userRouter.post('/signup', async (req, res) => {
+    const { email, password } = req.body
+
+    const user = await userModel.create({
+        email,
+        password
+    });
+
+    console.log(user);
+
+    if(user) {
+        res.json({
+            message: "signin succeded"
+        });
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
 
 });
 
-userRouter.post('/signin', (req, res) => {
+userRouter.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
 
+    const user = await userModel.findOne({
+        email,
+        password
+    })
+
+    if(user) {
+        const token = jwt.sign({
+            id: user._id
+        }, JWT_SECRET);
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: 'Incorrect crednetials'
+        })
+    }
 });
 
 module.exports = {
