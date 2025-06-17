cee46c683e9b3fdfb74303f59f91e42730480658
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-17 16:06:01 +0000

    added dependency and overall structure

--- taskified-project/backend/index.js
+++ taskified-project/backend/index.js
@@ -1,2 +1,16 @@
 const express = require('express');
+const app = express();
+const { userModel, todoModel } = require('./db');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const { todoRouter } = require('./routes/todo');
+const { userRouter } = require('./routes/user');
+
+mongoose.connect(process.env.MONGO_URL);
+app.use(express.json());
+
+app.use('/user', userRouter);
+app.use('/todo', todoRouter);
+
+app.listen(3000, () => console.log("Server start"));
 
---
+++ taskified-project/backend/routes/todo.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const todoRouter = Router();
+
+todoRouter.post('/', (req, res) => {
+
+});
+
+todoRouter.get('/', (req, res) => {
+
+});
+
+module.exports = {
+    todoRouter,
+}
