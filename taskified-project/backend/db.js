8ebeaabd648068419b6164e536fb7a0858f8a2f1
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-17 15:47:04 +0000

    DataBase created

---
+++ taskified-project/backend/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+const ObjectId = mongoose.ObjectId
+
+const User = new Schema({
+    email: { type: String, unique: true },
+    password: String,
+})
+
+const Todo = new Schema({
+    title: String,
+    category: String,
+    done: String
+})
+
+const userModel = mongoose.model('users', User);
+const todoModel = mongoose.model('todos', Todo);
+
+module.exports = {
+    userModel,
+    todoModel
+}
