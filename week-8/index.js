81d443092900989c3b41aea1b9536adc33590e4a
Atharv Nandrukar <atharvnandurkar@gmail.com>
2025-06-24 11:23:56 +0000

    routes added

--- week-8/index.js
+++ week-8/index.js
@@ -1,5 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const { mongoose } = require('mongoose');
 const app = express();
+app.use(express.json());
+const { adminRoute } = require('./routes/admin')
+const { courseRoute} = require('./routes/course')
+const { userRoute } = require('./routes/user')
+const db = require('./db');
 
-app.listen(3000);
+
+function main() {
+    mongoose.connect(process.env.MONGO_URL)
+    app.listen(3000);
+    console.log("Server Started");
+}
+main();
+
+app.use('/user', userRoute);
+app.use('/admin', adminRoute);
+app.use('/course', courseRoute);
+
 
